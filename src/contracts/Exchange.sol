58d7d1cc466c9b6a46bfe5fad604cebcfe7ab8e5
linhieng <790728740@qq.com>
2022-06-18 22:57:44 +0800

    完成了制作订单和取消订单，并完成了全部测试

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -14,8 +14,8 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 // [X] Deposit tokens 对应 depositToken
 // [X] Withdraw tokens
 // [X] Check balances
-// [ ] Make order
-// [ ] Cancel order
+// [X] Make order
+// [X] Cancel order
 // [ ] Fill order
 // [ ] Charge fees
 
@@ -27,10 +27,42 @@ contract Exchange {
   uint256 public feePercent; // the fee percentage
   address constant ETHER = address(0); // store Ether in tokens mapping with black address
   mapping(address => mapping(address => uint256)) public tokens; // 代币 => (实际用户地址 => 用户持有的代币数量)
+  mapping(uint256 => _Order) public orders; // a way store the order
+  uint public orderCount;
+  mapping(uint256 => bool) public orderCancelled;  // 注意取消订单的逻辑: 是新建一个 “已取消的订单” 映射, 而不是在订单中删除对应的订单
 
   // Event
   event Deposit(address token, address user, uint256 amount, uint256 balance);
-  event Withdraw(address token, address user, uint amount, uint balance);
+  event Withdraw(address token, address user, uint256 amount, uint256 balance);
+  event Order (
+    uint256 id,
+    address user,
+    address tokenGet,
+    uint256 amountGet,
+    address tokenGive,
+    uint256 amountGive,
+    uint256 timestamp
+  );
+  event Cancel (
+    uint256 id,
+    address user,
+    address tokenGet,
+    uint256 amountGet,
+    address tokenGive,
+    uint256 amountGive,
+    uint256 timestamp
+  );
+
+  // a way to model the order
+  struct _Order {
+    uint256 id;  // 订单的唯一 id
+    address user; // 发出订单的人
+    address tokenGet; // token address
+    uint256 amountGet;  // toknen amount
+    address tokenGive;  // eg: token give is ether
+    uint256 amountGive; // eg: ether amount
+    uint256 timestamp; // 订单时间戳
+  }
 
   constructor (address _feeAccount, uint256 _feePercent) public {
     feeAccount = _feeAccount;
@@ -77,5 +109,23 @@ contract Exchange {
   function balanceOf(address _token, address _user) public view returns (uint256) {
     return tokens[_token][_user];
   }
+
+  // add the order to storage
+  function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+    orderCount = orderCount.add(1);
+    orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+    emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+  }
+
+  function cancelOrder(uint256 _id) public {
+    _Order storage _order = orders[_id];  // storage 存在存储器中, 作为变量
+    // must be "my" order
+    require(address(_order.user) == msg.sender);
+    // must be a valid order: the order must exist
+    require(_order.id == _id);
+    orderCancelled[_id] = true;
+    emit Cancel(_order.id, msg.sender, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive, _order.timestamp);
+  }
+
 
 }
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -7,7 +7,7 @@ require('chai')
   .use(require('chai-as-promised'))
   .should()
 
-contract('Exchange', ([deployer, feeAccount, user1]) => {
+contract('Exchange', ([deployer, feeAccount, user1, user2]) => {
 
   let token
   let exchange
@@ -212,4 +212,90 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
     })
 
   })
+
+  describe('making orders', async () => {
+    let result
+
+    beforeEach(async () => {
+      result = await exchange.makeOrder(token.address, tokens(1), ETHER_ADDRESS, ether(1), { from: user1 })
+    })
+
+    it('tracks the newly created order', async () => {
+      const orderCount = await exchange.orderCount()
+      orderCount.toString().should.equal('1') // 1 是第一个订单的 id
+
+      const order = await exchange.orders('1') // 取出 id 为 1 的订单
+      order.id.toString().should.equal('1', 'id is correct')
+      order.user.should.equal(user1, 'user is correct')
+      order.tokenGet.should.equal(token.address, 'tokenGet is correct')
+      order.amountGet.toString().should.equal(tokens(1).toString(), 'amountGet is correct')
+      order.tokenGive.should.equal(ETHER_ADDRESS, 'tokenGive is correct')
+      order.amountGive.toString().should.equal(ether(1).toString(), 'amountGive is correct')
+      order.timestamp.toString().length.should.be.at.least(1, 'timestamp is present')
+    })
+    it('emits an "Order" event', async () => {
+      const log = result.logs[0]
+      log.event.should.eq('Order')
+
+      const event = log.args
+      event.id.toString().should.equal('1', 'id is correct')
+      event.user.should.equal(user1, 'user is correct')
+      event.tokenGet.should.equal(token.address, 'tokenGet is correct')
+      event.amountGet.toString().should.equal(tokens(1).toString(), 'amountGet is correct')
+      event.tokenGive.should.equal(ETHER_ADDRESS, 'tokenGive is correct')
+      event.amountGive.toString().should.equal(ether(1).toString(), 'amountGive is correct')
+      event.timestamp.toString().length.should.be.at.least(1, 'timestamp is present')
+    })
+  })
+
+  describe('order actions', async () => {
+    beforeEach(async () => {
+      // user1 deposits ether
+      await exchange.depositEther({ from: user1, value: ether(1) })
+      // user 1 makes an order to buy tokens with Ether
+      await exchange.makeOrder(token.address, tokens(1), ETHER_ADDRESS, ether(1), { from: user1 })
+    })
+
+    describe('cancelling orders', async () => {
+      let result
+
+      describe('success', async () => {
+        beforeEach(async () => {
+          result = await exchange.cancelOrder(1, { from: user1 })
+        })
+
+        it('updates cancelled orders', async () => {
+          const orderCancelled = await exchange.orderCancelled(1)
+          orderCancelled.should.equal(true)
+        })
+        it('emits a "Cancel" event', async () => {
+          const log = result.logs[0]
+          log.event.should.eq('Cancel')
+
+          const event = log.args
+          event.id.toString().should.equal('1', 'id is correct')
+          event.user.should.equal(user1, 'user is correct')
+          event.tokenGet.should.equal(token.address, 'tokenGet is correct')
+          event.amountGet.toString().should.equal(tokens(1).toString(), 'amountGet is correct')
+          event.tokenGive.should.equal(ETHER_ADDRESS, 'tokenGive is correct')
+          event.amountGive.toString().should.equal(ether(1).toString(), 'amountGive is correct')
+          event.timestamp.toString().length.should.be.at.least(1, 'timestamp is present')
+        })
+      })
+
+      describe('failure', async () => {
+        it('rejects invalid order ids', async () => {
+          const invalidOrderId = 99999
+          await exchange.cancelOrder(invalidOrderId, { from: user1 })
+            .should.be.rejectedWith(EVM_REVERT)
+        })
+        it('rejects unauthorized cancellations', async () => {
+          // Try to cancel the order from another user
+          await exchange.cancelOrder('1', { from: user2 })
+            .should.be.rejectedWith(EVM_REVERT)
+        })
+      })
+    })
+  })
+
 })
