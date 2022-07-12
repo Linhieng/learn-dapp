e74926be15ff4e066078093efcfd65b77bdb05c3
linhieng <790728740@qq.com>
2022-07-12 09:30:51 +0800

    改了点样式

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -244,8 +244,8 @@ class Balance extends Component {
   render() {
     return (
       <div className="card bg-dark text-white">
-        <div className="card-header">Balance </div>
-        <div className="card-body">
+        <div className="card-header">Balance 我的余额</div>
+        <div className="card-body hidden-overflow">
           {this.props.showForm ? showForm(this.props) : <Spinner />}
         </div>
       </div>
